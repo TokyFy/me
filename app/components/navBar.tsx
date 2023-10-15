e1bafda7a9f63b081bf8bf95ebb17a33534d1a19
TokyFy <tokyfydev@gmail.com>
2023-10-15 10:52:04 +0300

    Redesign the navBar

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -4,7 +4,13 @@ import React, {useEffect, useState} from 'react';
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 import Image from "next/image";
-import {Moon, Sun, Menu} from "lucide-react";
+import {
+    Moon,
+    Sun,
+    Menu,
+    ArrowUpRight,
+    Asterisk
+} from "lucide-react";
 
 function NavBar() {
 
@@ -16,29 +22,29 @@ function NavBar() {
     }
 
     return (
-        <div className="font-grotesk flex items-center text-[13px] font-bold border-b border-dashed border-b-neutral-200 py-4 px-8">
+        <div
+            className="font-grotesk flex justify-between items-center text-[13px] font-bold py-1  backdrop-blur-[2px]">
             <Link href={"/"}>
-                <div className="font-jost font-medium text-sm flex items-center gap-2 border-2 border-neutral-100 hover:bg-neutral-100 rounded-lg p-2">
-                    <div className={"rounded-md overflow-hidden border-neutral-200 bg-neutral-200"}>
-                        <Image className="scale-125" src={"/assets/noface.png"} alt={""} height={24} width={24}/>
-                    </div>
+                <div className="group font-jost font-medium text-md flex items-center text-neutral-100">
                     <div>
-                        Toky&apos;s Byte house
+                        <Asterisk className="group-hover:rotate-90" size={28}/>
                     </div>
                 </div>
             </Link>
 
-            <ul className="ml-auto justify-end items-center text-neutral-600 dark:text-neutral-400 flex-wrap w-max rounded-lg divide-x border-2 border-neutral-100 overflow-hidden hidden md:flex">
-                <NavItems route={"Me"} href={"/me/about"}/>
-                <NavItems route={"Writing"} href={"/me/writing"}/>
+            <ul className="justify-end items-center text-neutral-50 dark:text-neutral-400 gap-6 flex-wrap w-max border-neutral-100 overflow-hidden hidden md:flex px-2">
+                <NavItems route={"Feed"} href={"/me/writing"}/>
                 <NavItems route={"Projects"} href={"/me/projects"}/>
                 <NavItems route={"AMA"} href={"/me/guests"}/>
+                <NavItems route={"About"} href={"/me/about"}/>
+                <button
+                    onClick={() => switchTheme()}
+                    className="group cursor-pointer text-neutral-500 hover:border-neutral-700 hover:text-neutral-800 dark:hover:text-neutral-200 flex items-baseline"
+                >
+                    <p>{dark ? "Light" : "Dark"}</p>
+                    <ArrowUpRight className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5" size={12}/>
+                </button>
             </ul>
-
-            <button onClick={() => switchTheme()}
-                    className="p-2 bg-neutral-100 rounded-full cursor-pointer dark:bg-neutral-700 hidden md:ml-8 md:mr-0 md:flex">
-                {dark ? <Moon size={18}/> : <Sun size={18}/>}
-            </button>
 
             <div className="ml-auto md:hidden cursor-pointer">
                 <Menu/>
@@ -47,22 +53,24 @@ function NavBar() {
     );
 }
 
-export function NavItems({route , href} : {route : string , href ?: string}) {
+export function NavItems({route, href}: { route: string, href?: string }) {
 
     const pathname = usePathname()
 
     const [isActive, setIsActive] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         pathname === (href || route) ? setIsActive(true) : setIsActive(false);
     }, [pathname, href, route])
 
     return (
-        <li className={`cursor-pointer hover:underline hover:bg-lime-50 hover:text-neutral-800 dark:hover:text-neutral-200 px-6 py-2 ${isActive ? "bg-lime-50 text-neutral-700" : "text-neutral-700"}`} >
-            <Link href={href || route}>
-                {route}
-            </Link>
-        </li>
+        <Link href={href || route}>
+            <li className={`group cursor-pointer hover:text-neutral-100 dark:hover:text-neutral-200 flex items-baseline gap-1 border-b border-transparent  ${isActive ? "text-neutral-100" : "text-neutral-500"}`}>
+                <p>{route}</p>
+                <ArrowUpRight className="group-hover:translate-x-0.5 group-hover:-translate-y-0.5" size={12}/>
+            </li>
+        </Link>
+
     )
 }
 
